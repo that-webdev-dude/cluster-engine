2d46ebcb9adcc43025140064c3959b1aeaaabe81
that-webdev-dude <that.webdev.dude@gmail.com>
2022-06-05 10:15:08 +0100

    Loop completed

--- src/app.js
+++ src/app.js
@@ -2,14 +2,34 @@ import { Engine } from "./cluster";
 
 export default () => {
   // game setup
-  // ...
+  const canvas = document.querySelector("#display");
+  canvas.height = 640;
+  canvas.width = 832;
+  const ctx = canvas.getContext("2d");
+  ctx.globalAlpha = 0.02;
+  ctx.fillStyle = "#000";
 
   const gameLoop = new Engine(
+    // game update logic
     () => {
-      // game update logic
+      ctx.save();
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.globalAlpha = 1;
+      ctx.fillStyle = "#fff";
+
+      const y = Math.random() * canvas.height;
+      const x = Math.random() * canvas.width;
+      const radius = Math.random() * 20;
+
+      ctx.beginPath();
+      ctx.arc(x, y, radius, 0, Math.PI * 2);
+      ctx.fill();
+      ctx.restore();
     },
-    () => {
-      // game rendering
-    }
+    // game rendering
+    () => {}
   );
+
+  // start
+  gameLoop.start();
 };
