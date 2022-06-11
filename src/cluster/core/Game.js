00c69635a0b01e95d34342295d2a3009bb565c6c
that-webdev-dude <that.webdev.dude@gmail.com>
2022-06-11 07:02:48 +0100

    demo01 completed

---
+++ src/cluster/core/Game.js
@@ -0,0 +1,8 @@
+class Game {
+  constructor(width = 832, height = 640) {
+    this.height = height;
+    this.width = width;
+  }
+}
+
+export default Game;
--- src/cluster/index.js
+++ src/cluster/index.js
@@ -1,18 +1,20 @@
-import Engine from "./core/Engine";
-import Container from "./core/Container";
-import Texture from "./core/Texture";
-import Sprite from "./core/Sprite";
+import Game from "./core/Game";
 import Text from "./core/Text";
+import Engine from "./core/Engine";
+import Sprite from "./core/Sprite";
+import Texture from "./core/Texture";
+import Container from "./core/Container";
 import KeyControls from "./input/KeyControls";
 import MouseControls from "./input/MouseControls";
 import CanvasRenderer from "./renderers/CanvasRenderer";
 
 export default {
-  Engine,
-  Container,
-  Texture,
-  Sprite,
+  Game,
   Text,
+  Engine,
+  Sprite,
+  Texture,
+  Container,
   KeyControls,
   MouseControls,
   CanvasRenderer,
