48bd60e0105e922a6d17dbd0280ccf19a1ec6e2a
that-webdev-dude <that.webdev.dude@gmail.com>
2022-06-12 16:23:18 +0100

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,3 @@
-import buildingImageUrl from "./images/building.png";
-import playerImageUrl from "./images/player.png";
-
 import cluster from "./cluster/index.js";
 const { KeyControls } = cluster;
 const { Container } = cluster;
@@ -12,28 +9,13 @@ const { math } = cluster;
 
 export default () => {
   // setup
-  const width = 640;
-  const height = 300;
-  const game = new Game({ width, height });
+  const WIDTH = 640;
+  const HEIGHT = 300;
+  const game = new Game({ WIDTH, HEIGHT });
 
   // controller
-  const controller = new KeyControls();
 
   // game objects
-  const ship = game.scene.add(new Sprite(new Texture(playerImageUrl)));
-  ship.position.x = width / 2 - 16;
-  ship.position.y = height / 2;
-  ship.anchor.x = -16;
-  ship.anchor.y = -16;
-  ship.pivot.x = 16;
-  ship.pivot.y = 16;
-
-  ship.update = function (dt, t) {
-    let flipped = controller.action;
-    this.scale.x = flipped ? -1 : 1;
-    this.anchor.x = flipped ? 32 : 0;
-    ship.angle += 2;
-  };
 
   // start
   game.run((dt, t) => {});
