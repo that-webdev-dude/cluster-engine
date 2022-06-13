d3c389bcabe7d3c297caeeed48ae1c0dc563d92b
that-webdev-dude <that.webdev.dude@gmail.com>
2022-06-13 20:13:58 +0100

    main cleanup

--- src/app.js
+++ src/app.js
@@ -1,16 +1,8 @@
 import cluster from "./cluster/index.js";
-import Player from "./entities/Player";
-
-const { Texture } = cluster;
-const { Sprite } = cluster;
 const { Game } = cluster;
-const { math } = cluster;
 
 export default () => {
   // setup
-  const WIDTH = 640;
-  const HEIGHT = 300;
-  const game = new Game({ WIDTH, HEIGHT });
   const width = 640;
   const height = 320;
   const game = new Game({ width, height });
@@ -19,18 +11,13 @@ export default () => {
   // ...
 
   // controller
+  // ...
 
   // game objects
   // ...
 
   // game objects
-  const background = game.scene.add(new Sprite(new Texture(backgroundImageUrl)));
-
-  for (let i = 0; i < 100; i++) {
-    const player = game.scene.add(new Player());
-    player.position.x = math.rand(width - 32);
-    player.position.y = math.rand(height - 32);
-  }
+  // ...
 
   // start
   game.run((dt, t) => {});
