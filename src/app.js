60f44b5f3983adbada1ad27d5a4b205547429d8a
that-webdev-dude <that.webdev.dude@gmail.com>
2022-06-15 22:26:14 +0100

    random tilemap test

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
+import playerImageURL from "./images/player.png";
+import tilesImageURL from "./images/tiles.png";
+import levelImageURL from "./images/level.png";
 import cluster from "./cluster/index.js";
+import Texture from "./cluster/core/Texture";
+import math from "./cluster/utils/math";
+import TileMap from "./cluster/core/TileMap";
 const { Game } = cluster;
 
 export default () => {
@@ -10,8 +16,21 @@ export default () => {
   // controller
   // ...
 
+  // game level
+  const tileW = 32;
+  const tileH = 32;
+  const mapW = width / 32;
+  const mapH = height / 32;
+  const tiles = [];
+  for (let column = 0; column < mapW; column++) {
+    for (let row = 0; row < mapH; row++) {
+      tiles.push({ x: math.rand(512 / 32), y: math.rand(512 / 32) });
+    }
+  }
+  const map = new TileMap(tiles, mapW, mapH, tileW, tileH, new Texture(levelImageURL));
+
   // game objects
-  // ...
+  game.scene.add(map);
 
   // game logic
   game.run((dt, t) => {});
