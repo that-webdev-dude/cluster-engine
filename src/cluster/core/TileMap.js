3c1bbf76bba45c6b48e121c4e2d55913b36f1f4a
that-webdev-dude <that.webdev.dude@gmail.com>
2022-06-15 21:54:43 +0100

    TileMap class added

--- src/cluster/core/TileMap.js
+++ src/cluster/core/TileMap.js
@@ -1,5 +1,41 @@
-class TileMap {
-  constructor() {}
+import Container from "./Container";
+import TileSprite from "./TileSprite";
+
+class TileMap extends Container {
+  /**
+   * TileMap Is a Container that positions
+   * TileSprites into a grid
+   * @param {Array} tiles array of tile indexes
+   * @param {number} mapW width of the map in number of tiles (columns)
+   * @param {number} mapH height of the map in number of tiles (rows)
+   * @param {number} tileW width of a single tile
+   * @param {number} tileH height of a single tile
+   * @param {Texture} texture image source
+   */
+  constructor(tiles, mapW, mapH, tileW, tileH, texture) {
+    super();
+    this.mapW = mapW;
+    this.mapH = mapH;
+    this.tileW = tileW;
+    this.tileH = tileH;
+    this.children = tiles.map((frame, index) => {
+      const column = index % mapW;
+      const row = Math.floor(index / mapW);
+      const s = new TileSprite(texture, tileW, tileH);
+      s.frame = frame;
+      s.position.x = column * tileW;
+      s.position.y = row * tileH;
+      return s;
+    });
+  }
+
+  get width() {
+    return this.mapW * this.tileW;
+  }
+
+  get height() {
+    return this.mapH * this.tileH;
+  }
 }
 
 export default TileMap;
