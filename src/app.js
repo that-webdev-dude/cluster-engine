59dd63e389fd12dcf45ffebdc2beb928a1eee04c
that-webdev-dude <that.webdev.dude@gmail.com>
2022-08-07 17:11:24 +0100

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,4 @@
 import cluster from "./cluster/index.js";
-import LogoScreen from "./screens/LogoScreen.js";
-import TitleScreen from "./screens/TitleScreen.js";
-import GameScreen from "./screens/GameScreen.js";
-import GameoverScreen from "./screens/GameoverScreen.js";
 
 const { Game, KeyControls } = cluster;
 
@@ -12,20 +8,6 @@ export default () => {
     width: 640,
     height: 320,
   });
-
-  const newGame = () => {
-    game.scene = new GameScreen(game, controller, gameoverScreen);
-  };
-
-  const titleScreen = () => {
-    game.scene = new TitleScreen(game, controller, newGame);
-  };
-
-  const gameoverScreen = () => {
-    game.scene = new GameoverScreen(game, controller, titleScreen);
-  };
-
-  game.scene = new LogoScreen(game, titleScreen);
 
   game.run(() => {});
 };
