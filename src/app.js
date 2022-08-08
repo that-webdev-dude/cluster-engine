f157df5248fd06751aedd1c1add2ac41f668f419
that-webdev-dude <that.webdev.dude@gmail.com>
2022-08-08 22:17:04 +0100

    maze generator

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 import cluster from "./cluster/index.js";
+import Level from "./levels/Level.js";
 
 const { Game, KeyControls } = cluster;
+const GAME_WIDTH = 480 * 2 - 48;
+const GAME_HEIGHT = 480 - 48;
 
 export default () => {
   const controller = new KeyControls();
   const game = new Game({
-    width: 640,
-    height: 320,
+    width: GAME_WIDTH,
+    height: GAME_HEIGHT,
   });
+
+  const level = new Level({
+    width: GAME_WIDTH,
+    height: GAME_HEIGHT,
+  });
+
+  // console.log("file: app.js ~ line 16 ~ level", level);
+  game.scene.add(level);
 
   game.run(() => {
     // game update
---
+++ src/levels/Level.js
@@ -0,0 +1,84 @@
+import tilesImageURL from "../images/tiles.png";
+import cluster from "../cluster";
+
+const { Texture, TileMap, math } = cluster;
+const TILE_INDEXES = [
+  { id: "empty", x: 1, y: 2 },
+  { id: "wall", x: 2, y: 2 },
+  { id: "wall_end", x: 3, y: 2 },
+];
+
+const getTileIndexById = (id = "") => {
+  if (id.length > 0) {
+    return TILE_INDEXES.findIndex((tile) => tile.id === id);
+  } else {
+    return 0;
+  }
+};
+
+class Level extends TileMap {
+  constructor(
+    { width = 0, height = 0 } = {
+      width: 0,
+      height: 0,
+    }
+  ) {
+    // level setup
+    const texture = new Texture(tilesImageURL);
+    const tileW = 48;
+    const tileH = 48;
+    const mapW = width / tileW;
+    const mapH = height / tileH;
+    // procedural maze generator
+    const level = Array(mapW * mapH).fill(0);
+    for (let y = 0; y < mapH; y++) {
+      for (let x = 0; x < mapW; x++) {
+        // if has already a tile blick coming from the side walls really
+        if (level[y * mapW + x] === getTileIndexById("wall")) {
+          continue;
+        }
+        // if instead is a border tile, put a wall in it
+        if (x === 0 || y === 0 || x === mapW - 1 || y === mapH - 1) {
+          level[y * mapW + x] = getTileIndexById("wall");
+          continue;
+        }
+        // skip every other inner blocks add side walls
+        if (x % 2 || y % 2 || math.randOneIn(3)) {
+          continue;
+        } else {
+          level[y * mapW + x] = getTileIndexById("wall");
+          const [xo, yo] = math.randOneFrom([
+            [0, -1],
+            [1, 0],
+            [0, 1],
+            [-1, 0],
+          ]);
+          level[(y + yo) * mapW + (x + xo)] = getTileIndexById("wall");
+        }
+      }
+    }
+
+    // refine to add end-wall blocks
+    for (let y = 0; y < mapH - 1; y++) {
+      for (let x = 0; x < mapW; x++) {
+        if (level[y * mapW + x] === getTileIndexById("wall")) {
+          if (level[(y + 1) * mapW + x] === getTileIndexById("empty")) {
+            level[y * mapW + x] = getTileIndexById("wall_end");
+          }
+        }
+      }
+    }
+
+    // TileMap initialize
+    super(
+      level.map((i) => TILE_INDEXES[i]),
+      mapW,
+      mapH,
+      tileW,
+      tileH,
+      texture
+    );
+  }
+}
+
+export default Level;
