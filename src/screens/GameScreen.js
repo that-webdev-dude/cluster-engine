97e59a5d6b65e1f2fc9827b52f0e41d964641b67
that-webdev-dude <that.webdev.dude@gmail.com>
2022-09-07 16:36:52 +0100

    vector distance

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -3,36 +3,6 @@ import Vector from "../cluster/utils/Vector";
 import Dummy from "../entities/Dummy";
 import Timer from "../entities/Timer";
 
-// class Timer extends Text {
-//   constructor(positionVec = new Vector(0, 0)) {
-//     super("aaa", {
-//       font: "20px 'Press Start 2p'",
-//       fill: "red",
-//       align: "center",
-//     });
-
-//     this.time = 0;
-//     this.running = true;
-//     this.position.copy(positionVec);
-//   }
-
-//   stop() {
-//     this.running = false;
-//   }
-
-//   reset() {
-//     this.time = 0;
-//     this.running = true;
-//   }
-
-//   update(dt, t) {
-//     if (this.running) {
-//       this.time += dt;
-//       this.text = `${this.time.toFixed(3)}`;
-//     }
-//   }
-// }
-
 class GameScreen extends Container {
   constructor(game, controller) {
     super();
@@ -47,19 +17,20 @@ class GameScreen extends Container {
     this.initialize();
   }
 
-  // init
+  // test init
   initialize() {
     const { dummy, height } = this;
     dummy.position.set(0, height - 48);
   }
 
+  // test reset
   reset() {
     const { dummy, timer, height } = this;
     timer.reset();
     dummy.position.set(0, height - 48);
   }
 
-  // update
+  // test update
   update(dt, t) {
     super.update(dt, t);
     const { dummy, timer, width, height, controller } = this;
