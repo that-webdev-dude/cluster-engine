f7ed4ad09a7d33adb0d6df2618d12f5f30379daf
that-webdev-dude <that.webdev.dude@gmail.com>
2022-09-07 09:26:22 +0100

    ctd sprite

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ export default () => {
   const { Game, KeyControls } = cluster;
   const controller = new KeyControls();
   const game = new Game({
-    height: 48 * 10,
-    width: 48 * 20,
+    height: 48 * 10, // 960
+    width: 48 * 20, // 480
   });
 
   game.scene = new GameScreen(game, controller);
-  game.run(() => {});
+  // game.run(() => {});
 };
--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,9 +1,30 @@
 import Container from "../cluster/core/Container";
+import Vector from "../cluster/utils/Vector";
+import CrashTestDummy from "../entities/CrashTestDummy";
 
 class GameScreen extends Container {
-  constructor(game, controller) {}
+  constructor(game, controller) {
+    super();
+    const { width, height } = game;
+    const velocity = width / 3;
 
-  update(dt, t) {}
+    this.controller = controller;
+    this.height = height;
+    this.width = width;
+    this.ctd = this.add(new CrashTestDummy(velocity));
+    this.initialize();
+  }
+
+  // init
+  initialize() {
+    const { ctd, height } = this;
+    this.ctd.position.set(0, height - 48);
+  }
+
+  // update
+  update(dt, t) {
+    super.update(dt, t);
+  }
 }
 
 export default GameScreen;
