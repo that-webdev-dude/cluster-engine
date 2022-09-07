54d41382aa7ff63d5dbd127143cc5511891553e4
that-webdev-dude <that.webdev.dude@gmail.com>
2022-09-07 08:51:57 +0100

    platformer hold

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ import Vector from "./cluster/utils/Vector.js";
 
 const { Game, KeyControls } = cluster;
 
+const v1 = new Vector(100, 0);
+const v2 = new Vector(-100, 0);
+const dot = v1.dot(v2);
+console.log("file: app.js ~ line 10 ~ dot", dot);
+
 export default () => {
   const controller = new KeyControls();
   const game = new Game({
@@ -12,5 +17,5 @@ export default () => {
   });
 
   game.scene = new GameScreen(game, controller);
-  game.run(() => {});
+  // game.run(() => {});
 };
--- src/cluster/utils/Vector.js
+++ src/cluster/utils/Vector.js
@@ -31,7 +31,7 @@ class Vector {
 
   /**
    * copy the x, y props of the passed object
-   * @param {{X: Number, y: Number}} param0
+   * @param {{x: Number, y: Number}} param0
    * @returns this vector instance
    */
   copy({ x, y }) {
@@ -41,10 +41,20 @@ class Vector {
   }
 
   /**
+   * clone this vector
+   * and returns a new instance of it
+   * @param {{x: Number, y: Number}} param0
+   * @returns this vector instance
+   */
+  clone() {
+    return Vector.from(this);
+  }
+
+  /**
    * adds to this vector
    * the vector (object with x, y)
    * passed in as parameter
-   * @param {{X: Number, y: Number}} param0
+   * @param {{x: Number, y: Number}} param0
    * @returns this vector instance
    */
   add({ x, y }) {
@@ -57,13 +67,60 @@ class Vector {
    * subtract to this vector
    * the vector (object with x, y)
    * passed in as parameter
-   * @param {{X: Number, y: Number}} param0
+   * @param {{x: Number, y: Number}} param0
    * @returns this vector instance
    */
   subtract({ x, y }) {
     this.x -= x;
     this.y -= y;
     return this;
+  }
+
+  /**
+   * scale this vector
+   * by a scalar passed in as parameter
+   * @param {Number} scalar
+   * @returns this vector instance
+   */
+  scale(scalar = 1) {
+    this.x *= scalar;
+    this.y *= scalar;
+    return this;
+  }
+
+  /**
+   * alias to scale()
+   * scale this vector
+   * by a scalar passed in as parameter
+   * @param {Number} scalar
+   * @returns this vector instance
+   */
+  multiply(scalar = 1) {
+    return this.scale(scalar);
+  }
+
+  /**
+   * normalize this vector
+   * @returns this vector instance
+   */
+  normalize() {
+    let magnitude = this.magnitude;
+    if (magnitude > 0) {
+      this.x /= magnitude;
+      this.y /= magnitude;
+    }
+    return this;
+  }
+
+  /**
+   * computes the dot product between
+   * this vector and another vector
+   * passed in as parameter
+   * @param {{x: Number, y: Number}} param0
+   * @returns the dot product
+   */
+  dot({ x, y }) {
+    return this.x * x + this.y * y;
   }
 }
 
