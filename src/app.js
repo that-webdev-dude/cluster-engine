d32a61826262622eff2a9726fc4a6b1c36e49aae
that-webdev-dude <that.webdev.dude@gmail.com>
2022-09-07 08:57:27 +0100

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,15 +1,8 @@
-import cluster from "./cluster/index.js";
 import GameScreen from "./screens/GameScreen.js";
-import Vector from "./cluster/utils/Vector.js";
-
-const { Game, KeyControls } = cluster;
-
-const v1 = new Vector(100, 0);
-const v2 = new Vector(-100, 0);
-const dot = v1.dot(v2);
-console.log("file: app.js ~ line 10 ~ dot", dot);
+import cluster from "./cluster/index.js";
 
 export default () => {
+  const { Game, KeyControls } = cluster;
   const controller = new KeyControls();
   const game = new Game({
     height: 48 * 10,
@@ -17,5 +10,5 @@ export default () => {
   });
 
   game.scene = new GameScreen(game, controller);
-  // game.run(() => {});
+  game.run(() => {});
 };
