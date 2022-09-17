5530c8bb60457e2bd22881bffac58d3022370cba
that-webdev-dude <that.webdev.dude@gmail.com>
2022-09-17 17:58:49 +0100

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
-import GameScreen from "./screens/GameScreen.js";
-import cluster from "./cluster/index.js";
+// imports
 
 export default () => {
-  const { Game, KeyControls } = cluster;
-  const input = new KeyControls();
-  const game = new Game({
-    width: 960,
-    height: 480,
-  });
-
-  game.scene = new GameScreen(game, input);
-  game.run(() => {});
+  // game init
 };
