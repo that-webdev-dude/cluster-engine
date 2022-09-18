2c00b381736cf4dbdd6c4552fef7e6c3fd5f1c4a
that-webdev-dude <that.webdev.dude@gmail.com>
2022-09-18 09:16:47 +0100

    matter loaded

--- src/app.js
+++ src/app.js
@@ -1,5 +1,50 @@
 // imports
+import Matter from "../vendor/matter";
 
 export default () => {
-  // game init
+  const { Engine, Render, Runner, Bodies, Composite, World } = Matter;
+  const height = 600;
+  const width = 800;
+
+  // game init !!!!!
+  const engine = Engine.create();
+
+  // bodies !!!!!
+  // prettier-ignore
+  const course =  Bodies.rectangle(
+    width / 2, 
+    height / 2, 
+    width - 100, 
+    48, 
+    { isStatic: true } 
+  );
+
+  const balls = Array(50)
+    .fill(0)
+    .map((b) => {
+      const radius = Math.random() * 25 + 5;
+      const x = Math.random() * width;
+      const y = Math.random() * -1000;
+      const options = {
+        restitution: 0.7,
+      };
+
+      return Bodies.circle(x, y, radius, options);
+    });
+
+  // world !!!!!
+  World.add(engine.world, [course, ...balls]);
+  Runner.run(engine);
+
+  // debug render !!!!!
+  const render = Render.create({
+    element: document.querySelector("#app"),
+    engine: engine,
+    options: {
+      width: width,
+      height: height,
+      showAngleIndicator: true,
+    },
+  });
+  Render.run(render);
 };
