b8add11009772898bc10a14c211373fc4795c87d
that-webdev-dude <that.webdev.dude@gmail.com>
2022-12-06 22:38:09 +0000

    sound setters

--- src/cluster/sound/Sound.js
+++ src/cluster/sound/Sound.js
@@ -2,10 +2,13 @@ class Sound {
   constructor(src, options = {}) {
     this.src = src;
     this.options = Object.assign({ volume: 1 }, options);
+    this.playing = false;
 
-    // ... config audio element
+    // config audio element
     this.audio = new Audio();
     this.audio.src = src;
+
+    // audio element listeners
     this.audio.addEventListener(
       "error",
       () => {
@@ -13,19 +16,52 @@ class Sound {
       },
       false
     );
+    this.audio.addEventListener(
+      "ended",
+      () => {
+        console.log(this.playing);
+      },
+      false
+    );
   }
 
+  /**
+   * @getter volume
+   */
+  get volume() {
+    return this.audio.volume;
+  }
+
+  /**
+   * @setter volume
+   */
+  set volume(volume) {
+    this.options.volume = volume;
+    this.audio.volume = volume;
+  }
+
+  /**
+   * play
+   * @param {*} overrides
+   * @return void
+   */
   play(overrides = {}) {
     const { audio, options } = this;
     const opts = Object.assign({ time: 0 }, options, overrides);
     audio.volume = opts.volume;
     audio.time = opts.time;
     audio.play();
+    this.playing = true;
   }
 
+  /**
+   * stop
+   * @return void
+   */
   stop() {
     const { audio } = this;
     audio.pause();
+    this.playing = false;
   }
 }
 
