b96fad372f02dfa02483ce222648f392595ae4f6
that-webdev-dude <that.webdev.dude@gmail.com>
2022-12-22 23:14:52 +0000

    timing

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import cluster from "./cluster";
 import Assets from "./cluster/core/Assets";
-import plopSoundURL from "./sounds/plop.mp3";
+import themeSoundURL from "./sounds/theme.mp3";
 
 export default () => {
-  const { Game, KeyControls, MouseControls, Audio, SoundBuffer } = cluster;
+  const { Game, KeyControls, MouseControls, SoundBuffer } = cluster;
   const height = 640;
   const width = 832;
   const game = new Game({ width: width, height: height });
@@ -12,22 +12,12 @@ export default () => {
     mouse: new MouseControls(game.view),
   };
 
-  const sound = new SoundBuffer(plopSoundURL, Audio.context);
-
-  function highpassFilter(context) {
-    // https://webaudio.github.io/web-audio-api/#enumdef-biquadfiltertype
-    const filter = context.createBiquadFilter();
-    filter.type = "highpass";
-    filter.frequency.value = 2660;
-    filter.Q.value = 25;
-    return filter;
-  }
-  const highpass = highpassFilter(Audio.context);
+  const themeSoundBuffer = new SoundBuffer(themeSoundURL);
 
   Assets.onReady(() => {
     game.run((dt, t) => {
       if (input.key.action) {
-        sound.play({ speed: 1, filter: highpass, delay: 2 });
+        themeSoundBuffer.play();
       }
     });
   });
--- src/cluster/sound/SoundBuffer.js
+++ src/cluster/sound/SoundBuffer.js
@@ -11,7 +11,7 @@ class SoundBuffer {
         volume: 1,
         time: 0,
         speed: 1,
-        delay: 1,
+        delay: 0,
         filter: null,
       },
       options
@@ -23,11 +23,16 @@ class SoundBuffer {
     if (!this.playing) {
       const { audio, options } = this;
       const opts = Object.assign(options, overrides);
+      const now = Audio.context.currentTime;
       audio.then((buffer) => {
         const source = Audio.context.createBufferSource();
         source.buffer = buffer;
         source.volume = opts.volume;
         source.playbackRate.value = opts.speed;
+        source.onended = () => {
+          this.playing = false;
+        };
+
         if (opts.filter) {
           const { filter } = opts;
           source.connect(filter);
@@ -35,12 +40,8 @@ class SoundBuffer {
         } else {
           source.connect(opts.output);
         }
-        source.onended = () => {
-          this.playing = false;
-        };
 
-        source.start(Audio.context.currentTime + opts.delay);
-        // source.start(0, opts.time);
+        source.start(now + opts.delay);
         this.playing = true;
       });
     }
@@ -48,3 +49,47 @@ class SoundBuffer {
 }
 
 export default SoundBuffer;
+
+/**
+ * example of an highpass filter
+ * -----------------------------
+ */
+// function highpassFilter(context) {
+//   // https://webaudio.github.io/web-audio-api/#enumdef-biquadfiltertype
+//   const filter = context.createBiquadFilter();
+//   filter.type = "highpass";
+//   filter.frequency.value = 2660;
+//   filter.Q.value = 25;
+//   return filter;
+// }
+// const highpass = highpassFilter(Audio.context);
+
+/**
+ * example of an oscillator
+ * ------------------------
+ */
+// const { context, master } = Audio;
+// const bpm = 60 / 250;
+// const note = 25;
+// const playNote = (note, startTime, length) => {
+//   const oscillator = context.createOscillator();
+//   oscillator.type = "sawtooth";
+//   oscillator.frequency.value = note;
+//   oscillator.connect(master);
+//   oscillator.start(startTime);
+//   oscillator.stop(startTime + length);
+// };
+// playNote(note, 0, 1);
+
+/**
+ * example of fadeIn function
+ * --------------------------
+ */
+// function fade(to, length) {
+//   const now = context.currentTime;
+//   master.gain.setValueAtTime(master.gain.value, now);
+//   master.gain.linearRampToValueAtTime(to, now + length);
+// }
+
+// master.gain.value = 0;
+// fade(1, 1);
