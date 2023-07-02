ce712397681f8b537744ce955752aad1d1b5ee1b
that.webdev.dude <that.webdev.dude@gmail.com>
2023-07-02 17:23:27 +0100

    renderer canvas handler

--- src/cluster/renderers/CanvasRenderer.js
+++ src/cluster/renderers/CanvasRenderer.js
@@ -144,6 +144,15 @@ class CanvasRenderer {
    * @param {Container} container
    */
   #renderRecursive(container, camera = null) {
+    // const e = document.createElement("canvas");
+    // e.width = 100;
+    // e.height = 100;
+    // const c = e.getContext("2d");
+    // c.fillStyle = "red";
+    // c.fillRect(0, 0, 100, 100);
+    // this.context.drawImage(e, 0, 0);
+    // console.log(e.style);
+
     // exit immediately if the whole container is transparent
     if (container.visible === false || container.alpha === 0) return;
 
@@ -159,7 +168,6 @@ class CanvasRenderer {
 
       // exit immediately if the whole container offscreen
       if (camera && !isOnCamera(child, camera)) {
-        console.log("not rendering");
         return;
       }
 
@@ -216,6 +224,8 @@ class CanvasRenderer {
         } else if (child.path) {
           CanvasArtist.drawPath(context, child);
         }
+      } else if (child.canvas) {
+        context.drawImage(child.canvas, 0, 0);
       }
 
       if (child.children) {
--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -24,13 +24,31 @@ class Player extends Rect {
   }
 }
 
+class Spiral {
+  constructor(position = new Vector()) {
+    const e = document.createElement("canvas");
+    e.width = 100;
+    e.height = 100;
+    const c = e.getContext("2d");
+    c.fillStyle = "red";
+    c.fillRect(0, 0, 100, 100);
+
+    this.width = 200;
+    this.height = 200;
+    this.position = position;
+    this.canvas = e;
+  }
+}
+
 class GameScreen extends Screen {
   constructor(game, input) {
     super(game, input);
+    const spiral = new Spiral(new Vector(100, 100));
     const player = new Player(input);
     const camera = new Camera(player, game.view);
 
     camera.add(player);
+    camera.add(spiral);
 
     this.player = player;
     this.camera = this.add(camera);
