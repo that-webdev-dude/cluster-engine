456e7eca6c070526dd0280416924fa82ea5fd8b0
that.webdev.dude <that.webdev.dude@gmail.com>
2023-11-26 16:59:31 +0000

    sound setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import ares from "./ares";
 import Player from "./entities/Player";
+import LaserShootSoundURL from "./sounds/Laser_Shoot.wav";
 
 const game = new ares.Game({
   version: "0.0.1",
@@ -8,13 +9,55 @@ const game = new ares.Game({
   height: 640,
 });
 
-// const player = new Player({
-//   input: game.keyboard,
-//   onFire: () => {
-//     console.log("fire");
-//   },
-// });
-// game.scene.add(player);
+class Sound {
+  private context: AudioContext;
+  private gainNode: GainNode;
+  private buffer: AudioBuffer | null = null;
+  private source: AudioBufferSourceNode | null = null;
+  private isLooping: boolean = false;
+
+  constructor(private url: string) {
+    this.context = new AudioContext();
+    this.gainNode = this.context.createGain();
+    this.gainNode.connect(this.context.destination);
+    this.loadSound();
+  }
+
+  private async loadSound() {
+    const response = await fetch(this.url);
+    const arrayBuffer = await response.arrayBuffer();
+    this.buffer = await this.context.decodeAudioData(arrayBuffer);
+  }
+
+  public play() {
+    if (!this.buffer) return;
+    this.source = this.context.createBufferSource();
+    this.source.buffer = this.buffer;
+    this.source.loop = this.isLooping;
+    this.source.connect(this.gainNode);
+    this.source.start();
+  }
+
+  public stop() {
+    if (!this.source) return;
+    this.source.stop();
+    this.source = null;
+  }
+
+  set loop(value: boolean) {
+    this.isLooping = value;
+    if (this.source) {
+      this.source.loop = value;
+    }
+  }
+
+  set volume(value: number) {
+    this.gainNode.gain.value = value;
+  }
+}
+
+const fireSound = new Sound(LaserShootSoundURL);
+console.log("file: app.ts:60 ~ fireSound:", fireSound);
 
 export default () => {
   game.start(() => {});
