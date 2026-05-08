e8faa8a3725c35a929dc0b5eb7661e7ce37142b5
that.webdev.dude <that.webdev.dude@gmail.com>
2026-05-08 21:41:09 +0100

    feat: add initial implementation of checkArrays and index files

---
+++ src/checkArrays.ts
@@ -0,0 +1,13 @@
+export default () => {
+    const buffer = new ArrayBuffer(16);
+};
+
+// simple sprite model
+// float32 px
+// float32 py
+// float32 w
+// float32 h
+// float32 u
+// float32 v
+// float32 uvW
+// float32 uvH
